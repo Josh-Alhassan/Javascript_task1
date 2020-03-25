3971d1b0862af1558326c5b94ff7af16ddfaaad8
saint-josh <saintjosh26@gmail.com>
2020-03-25 01:53:34 +0100

    Javascript-task1

---
+++ Task1.js
@@ -0,0 +1,13 @@
+name = "Alhassan Joshua Abel";
+courses = ['HTML', 'CSS', 'Javascript'];
+
+//Displaying my name and courses
+console.log(name)
+console.log(courses)
+
+//Number 2
+for (courses = 0; courses <= 200; courses++) {
+    if ( courses % 2 == 0) {
+        console.log(courses)
+    }
+}
